041d8d726fd8ae55dffff061d32db6e4c100a057
Michael M. Vitousek <mvitouse@indiana.edu>
2016-05-11 16:05:10 -0400

    one more fix

--- Typed/Utilities.py
+++ Typed/Utilities.py
@@ -5,7 +5,7 @@ this_package_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(this_package_path, '..'))
 
 #TODO: Cannot type variable in retic
-data = (list(map(int, [line.strip() for line in open(
+data = (list(map(float, [line.strip() for line in open(
     "utill-random-numbers.txt")])))
 rand_num = (element for element in data)
 
